92f1f69ab1d14eb36e56bc41cf84af77a7aeeb7d
hsiong <peak1203@126.com>
2023-04-21 18:06:30 +0800

    upgrade color config

--- oh-my-zsh/color/config_color.sh
+++ oh-my-zsh/color/config_color.sh
@@ -1,3 +1,7 @@
 
 # local ~/project-init-ubuntu/oh-my-zsh/color
 scp -r ./* user@ip:/root/.oh-my-zsh/custom/plugins
+
+# local copy
+cd /remoteDir/zsh/color 
+cp color.sh	~/.oh-my-zsh/custom/plugins
